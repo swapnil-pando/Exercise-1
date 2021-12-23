0fb1faf960ad43e41a4f0c1f0b34d2ca4362643e
Swapnil Satpathy <swapnilsatpathy@pando.ai>
2021-12-23 14:45:59 +0530

    Refactor Code - Remove the Code for uploaded file checking to a different module

---
+++ file_upload_checks.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const fileExists=(file_name)=>{
+    // console.log("Going In,"+ fs.existsSync(file_name));
+    return fs.existsSync(file_name);
+}
+
+const shaExists=(shas,sha_value)=>{
+    return (shas.indexOf(sha_value) !== -1);
+}
+
+module.exports = {
+    fileExists:fileExists,
+    shaExists:shaExists
+};
